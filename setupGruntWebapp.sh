8c8d450466919a7f755f13f2165402b7236a5c10
rosso <rosso@debian.home>
2014-07-12 16:58:31 +1000

    added files customised for me

---
+++ setupGruntWebapp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo npm install -g express
+
+sudo npm install -g grunt-cli
+sudo npm install -g grunt-init
+echo sudo npm install -g phantomjs
+sudo npm install -g bower
+sudo npm install -g yeoman
+
+
+
+
